e4e45517a9262165e02a0ad9088d84c70c9e0b1f
dang12394 <dang12394@gmail.com>
2024-08-06 16:55:59 +0700

    fix web

--- web.tf
+++ web.tf
@@ -1,24 +1,19 @@
-resource "azurerm_app_service_plan" "swa" {
+resource "azurerm_service_plan" "swa" {
   name                = "dang12394-asp"
   location            = azurerm_resource_group.swa.location
   resource_group_name = azurerm_resource_group.swa.name
-  kind                = "App"
-  reserved            = true
+  sku_name            = "F1"
+  os_type             = "Windows"
 
-  sku {
-    tier = "Free"
-    size = "F1"
-    capacity = 1
-  }
 }
 
-resource "azurerm_app_service" "swa" {
+resource "azurerm_windows_web_app" "swa" {
   name                = "dang12394"
   location            = azurerm_resource_group.swa.location
   resource_group_name = azurerm_resource_group.swa.name
-  app_service_plan_id = azurerm_app_service_plan.swa.id
+  service_plan_id = azurerm_service_plan.swa.id
   site_config {
-    linux_fx_version = "DOTNETCORE|8.0"
+    windows_fx_version = "DOTNETCORE|8.0"
   }
   app_settings = {
     ASPNETCORE_ENVIRONMENT = "Development",
